af3edf5b411a6772d4add93385224ecace91d07b
imaun <imun22@gmail.com>
2022-09-01 17:55:47 +0430

    Add ContentCategory Create method

--- src/Behlog.Cms/ContentCategories/ContentCategory.cs
+++ src/Behlog.Cms/ContentCategories/ContentCategory.cs
@@ -6,10 +6,30 @@ namespace Behlog.Cms;
 public class ContentCategory : AggregateRoot<Guid> 
 {
 
-    protected ContentCategory() : base()
+    protected ContentCategory(CreateContentCategoryArg args) : base()
     {
+        if(args is null) throw new ArgumentNullException(nameof(args));
 
+        Id = Guid.NewGuid();
+        Title = args.Title;
+        AltTitle = args.AltTitle;
+        Slug = args.Slug;
+        ParentId = args.ParentId;
+        Description = args.Description;
+        ContentTypeId = args.ContentTypeId;
+        Status = EntityStatus.Enabled;
     }
+
+
+    #region Methods
+
+    public static ContentCategory Create(CreateContentCategoryArg args)
+    {
+        var category = new ContentCategory(args);
+        return category;
+    }
+
+    #endregion
 
     #region Props
     public string Title { get; }
