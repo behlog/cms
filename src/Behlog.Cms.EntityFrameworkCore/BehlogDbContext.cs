7c587a490caf039ca46ca5f3eb47de1f95e895ec
imun <imun22@gmail.com>
2022-10-29 15:35:26 +0330

    Implement BehlogDbContext

--- src/Behlog.Cms.EntityFrameworkCore/BehlogDbContext.cs
+++ src/Behlog.Cms.EntityFrameworkCore/BehlogDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Behlog.Core;
+using Behlog.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
@@ -10,56 +11,113 @@ public class BehlogDbContext : DbContext, IBehlogDbContext
 
     public BehlogDbContext(DbContextOptions options): base(options)
     {
-        
     }
     
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _transaction?.Dispose();
+        base.Dispose();
     }
 
     public void BeginTransaction()
     {
-        throw new NotImplementedException();
+        _transaction = Database.BeginTransaction();
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        _transaction = await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
     }
 
     public void RollbackTransaction()
     {
-        throw new NotImplementedException();
+        if (_transaction is null)
+        {
+            throw new NullReferenceException(
+                "The transaction is null. You have to call 'BeginTransaction' method first.");
+        }
+        _transaction.Rollback();
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_transaction is null)
+        {
+            throw new NullReferenceException(
+                "The transaction is null. You have to call 'BeginTransactionAsync' method first.");
+        }
+
+        await _transaction.RollbackAsync(cancellationToken);
     }
 
     public void CommitTransaction()
     {
-        throw new NotImplementedException();
+        if (_transaction is null)
+        {
+            throw new NullReferenceException(
+                "The transaction is null. You have to call 'BeginTransaction' method first.");
+        }
+        
+        _transaction.Commit();
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_transaction is null)
+        {
+            throw new NullReferenceException(
+                "The transaction is null. You have to call 'BeginTransactionAsync' method first.");
+        }
+
+        await _transaction.CommitAsync(cancellationToken).ConfigureAwait(false);
     }
 
     public int SaveChanges()
     {
-        throw new NotImplementedException();
+        ChangeTracker.DetectChanges();
+        ChangeTracker.AutoDetectChangesEnabled = false;
+        var result = base.SaveChanges();
+        ChangeTracker.AutoDetectChangesEnabled = true;
+        return result;
     }
 
-    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        ChangeTracker.DetectChanges();
+        ChangeTracker.AutoDetectChangesEnabled = false;
+        var result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        ChangeTracker.AutoDetectChangesEnabled = true;
+        return result;
     }
 
     public void ExecuteRawCommand(string query, params object[] parameters)
     {
-        throw new NotImplementedException();
+        Database.ExecuteSqlRaw(query, parameters);
+    }
+
+    public async Task ExecuteRawCommandAsync(
+        string query, object[] parameters, CancellationToken cancellationToken = default)
+    {
+        await Database.ExecuteSqlRawAsync(query, parameters, cancellationToken).ConfigureAwait(false);
     }
 
     public bool EnsureCreated()
     {
-        throw new NotImplementedException();
+        return base.Database.EnsureCreated();
+    }
+
+    public async Task<bool> EnsureCreatedAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.Database.EnsureCreatedAsync(cancellationToken).ConfigureAwait(false);
     }
 
     public void MigrateDb()
     {
-        throw new NotImplementedException();
+        Database.Migrate();
     }
 
-    public Task MigrateDbAsync(CancellationToken cancellationToken = new CancellationToken())
+    public async Task MigrateDbAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await Database.MigrateAsync(cancellationToken).ConfigureAwait(false);
     }
 }
