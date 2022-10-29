59cfb2a17bea4e2f74d6712b8a37e83bd40bf06f
imun <imun22@gmail.com>
2022-10-29 18:03:55 +0330

    Add file

---
+++ src/Behlog.Cms.Domain/Files/File.cs
@@ -0,0 +1,17 @@
+using Behlog.Core.Domain;
+
+namespace Behlog.Cms.Domain;
+
+public class File : BehlogEntity<Guid>
+{
+
+    private File()
+    {
+    }
+
+    #region props
+
+    
+
+    #endregion
+}
