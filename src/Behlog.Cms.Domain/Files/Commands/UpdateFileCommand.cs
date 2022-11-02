3ea1a56b55a0e4bf6740aa13bf23f54974a2382f
imun <imun22@gmail.com>
2022-11-02 17:02:55 +0330

    Add UpdateFileCommand

---
+++ src/Behlog.Cms.Domain/Files/Commands/UpdateFileCommand.cs
@@ -0,0 +1,12 @@
+using Behlog.Core;
+
+namespace Behlog.Cms.Commands;
+
+public class UpdateFileCommand : IBehlogCommand
+{
+    public Guid Id { get; }
+    public string Title { get; }
+    public string AltTitle { get; }
+    public bool Enabled { get; }
+    public string Description { get; }
+}
