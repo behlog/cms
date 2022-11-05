589c204e8d8d1fce5d1a7ab888f5f5e28a71b6d2
imun <imun22@gmail.com>
2022-11-05 15:25:22 +0330

    Fix ContentMeta to inherit from MetaBase class in Behlog.Core

--- src/Behlog.Cms.Domain/Contents/ContentMeta.cs
+++ src/Behlog.Cms.Domain/Contents/ContentMeta.cs
@@ -1,26 +1,26 @@
 using Behlog.Core;
-using Behlog.Core.Domain;
+using Behlog.Core.Models;
 
 namespace Behlog.Cms.Domain;
 
-public class ContentMeta : ValueObject
+public class ContentMeta : MetaBase<Guid>
 {
     public ContentMeta() { }
 
     public ContentMeta(
-        Guid contentId, string metaKey, string value, 
-        EntityStatus status, string description, string category, int orderNum)
+        Guid ownerId, string metaKey, string metaValue, 
+        EntityStatus status, string description, string category, 
+        int orderNum)
     {
-        ContentId = contentId;
+        OwnerId = ownerId;
         MetaKey = metaKey;
-        Value = value;
+        Value = metaValue;
         Status = status;
         Description = description;
         Category = category;
         OrderNum = orderNum;
     }
     
-    public Guid ContentId { get; private set; }
     public string MetaKey { get; private set; }
     public string Value { get; private set; }
     public EntityStatus Status { get; private set; }
@@ -28,14 +28,4 @@ public class ContentMeta : ValueObject
     public string Category { get; private set; }
     public int OrderNum { get; private set; }
     
-    protected override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return ContentId;
-        yield return MetaKey;
-        yield return Value;
-        yield return Status;
-        yield return Description;
-        yield return Category;
-        yield return OrderNum;
-    }
 }
