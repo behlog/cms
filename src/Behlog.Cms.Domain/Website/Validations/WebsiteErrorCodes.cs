a6f888fe973d6e21b3b128add1c4a5cb36c705fb
imaun <imun22@gmail.com>
2022-11-17 00:01:02 +0330

    Add ErrorCodes for Website

---
+++ src/Behlog.Cms.Domain/Website/Validations/WebsiteErrorCodes.cs
@@ -0,0 +1,22 @@
+namespace Behlog.Cms.Handlers;
+
+public static class WebsiteErrorCodes
+{
+
+    public static string NameIsNull = "website.name.null";
+    public static string NameMaxLen = "website.name.maxlen";
+    public static string OwnerUserNull = "website.owneruser.null";
+    public static string TitleIsNull = "website.title.null";
+    public static string TitleMaxLen = "website.title.maxlen";
+    public static string DescriptionMaxLen = "website.desc.maxlen";
+    public static string KeywordsMaxLen = "website.keywords.maxlen";
+    public static string UrlMaxLen = "website.url.maxlen";
+    public static string InvalidStatus = "webiste.status.invalid";
+    public static string EmailFormat = "website.email.format";
+    public static string CopyrightMaxLen = "website.cpr.maxlen";
+
+    public static string GetMessage(string errorCode)
+    {
+        return "";
+    }
+}
