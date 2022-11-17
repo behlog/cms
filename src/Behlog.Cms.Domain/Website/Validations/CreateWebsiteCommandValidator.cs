860a009e421ac00b07352bd20df63b45372556c4
imaun <imun22@gmail.com>
2022-11-17 20:21:32 +0330

    Refactor Validate CreateWebsiteCommand

--- src/Behlog.Cms.Domain/Website/Validations/CreateWebsiteCommandValidator.cs
+++ src/Behlog.Cms.Domain/Website/Validations/CreateWebsiteCommandValidator.cs
@@ -19,25 +19,44 @@ public class CreateWebsiteCommandValidator
         
         return ValidatorResult.Create()
             .HasMaxLenght(command.Name, 256, nameof(command.Name), 
-                "Name maxlen is 256", WebsiteErrorCodes.NameMaxLen)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.NameMaxLen)!, 
+                WebsiteErrorCodes.NameMaxLen)
+            
             .IsNotNullOrEmpty(command.Name, nameof(command.Name), 
-                "Name can't be null", WebsiteErrorCodes.NameIsNull)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.NameIsNull)!, 
+                WebsiteErrorCodes.NameIsNull)
+            
             .IsNotNullOrEmpty(command.Title, nameof(command.Title), 
-                "Title can't be null.", WebsiteErrorCodes.TitleIsNull)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.TitleIsNull)!, 
+                WebsiteErrorCodes.TitleIsNull)
+            
             .HasMaxLenght(command.Description, 2000, nameof(command.Description),
-                "Description Maxlen is 2000", WebsiteErrorCodes.DescriptionMaxLen)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.DescriptionMaxLen)!,
+                WebsiteErrorCodes.DescriptionMaxLen)
+            
             .HasMaxLenght(command.Keywords, 1000, nameof(command.Keywords), 
-                "Kmeywords MaxLen is 1000", WebsiteErrorCodes.KeywordsMaxLen)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.KeywordsMaxLen)!, 
+                WebsiteErrorCodes.KeywordsMaxLen)
+            
             .HasMaxLenght(command.Url, 2000, nameof(command.Url), 
-                "Url len is 2000", WebsiteErrorCodes.UrlMaxLen)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.UrlMaxLen)!, 
+                WebsiteErrorCodes.UrlMaxLen)
+            
             .IsNotNullOrEmpty(command.OwnerUserId, nameof(command.OwnerUserId), 
-                "OwnerUserId null", WebsiteErrorCodes.OwnerUserNull)
-            .CheckWebsiteStatusOnCreate(WebsiteStatus.UnderConstruction, 
-                "", WebsiteErrorCodes.InvalidStatus)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.OwnerUserNull)!, 
+                WebsiteErrorCodes.OwnerUserNull)
+            
+            .CheckWebsiteStatusOnCreate(WebsiteStatus.UnderConstruction,
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.InvalidStatus)!,
+                WebsiteErrorCodes.InvalidStatus)
+            
             .IsEmailFormatCorrect(command.Email, nameof(command.Email), 
-                "Email format is incorrect", WebsiteErrorCodes.EmailFormat)
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.EmailFormat)!, 
+                WebsiteErrorCodes.EmailFormat)
+            
             .HasMaxLenght(command.CopyrightText, 1000, nameof(command.CopyrightText), 
-                ValidationMessages.website_cpr_maxlen, WebsiteErrorCodes.CopyrightMaxLen);
+                WebsiteErrorCodes.GetMessage(WebsiteErrorCodes.CopyrightMaxLen)!, 
+                WebsiteErrorCodes.CopyrightMaxLen);
     }
 
 
