cef54887d68f4b2af7a152b372a5cc0c5ee73eee
imun <imun22@gmail.com>
2022-11-29 15:04:01 +0330

    Add ContentFileErrorCodes

--- src/Behlog.Cms/Contents/Validations/ContentErrorCodes.cs
+++ src/Behlog.Cms/Contents/Validations/ContentErrorCodes.cs
@@ -3,7 +3,7 @@ using Behlog.Cms.Resources;
 namespace Behlog.Cms.Errors;
 
 
-public class ContentErrorCodes
+public static class ContentErrorCodes
 {
     public static string TitleIsNull = "content.title.null";
     public static string TitleMaxLen = "content.title.maxlen";
@@ -22,7 +22,7 @@ public class ContentErrorCodes
 }
 
 
-public class MetaErrorCodes
+public static class MetaErrorCodes
 {
     public static string KeyIsNull = "meta.key.null";
     public static string TitleMaxLen = "meta.title.maxlen";
@@ -38,11 +38,27 @@ public class MetaErrorCodes
 }
 
 
-public class LikeErrorCodes
+public static class LikeErrorCodes
 {
     public static string SessionIdMaxLen = "like.session.maxlen";
     public static string UserIdMaxLen = "like.user.maxlen";
     public static string IpAddressMaxLen = "like.ip.maxlen";
+    
+    public static string? GetMessage(string errorCode)
+    {
+        return ValidationMessages.ResourceManager.GetString(errorCode);
+    }
+}
+
+
+public static class ContentFileErrorCodes
+{
+    public static string FileIdInvalid = "content.file.id.invalid";
+    public static string FileNameIsNull = "content.file.name.null";
+    public static string FileNameMaxLen = "content.file.name.maxlen";
+    public static string TitleMaxLen = "content.title.maxlen";
+    public static string DescriptionMaxLen = "content.desc.maxlen";
+    
     
     public static string? GetMessage(string errorCode)
     {
