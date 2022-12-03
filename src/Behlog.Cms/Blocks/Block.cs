b6eafaf201c902c91d36a22600b22a69ac7e852e
imun <imun22@gmail.com>
2022-12-03 17:47:22 +0330

    Add Block.Create builder method

--- src/Behlog.Cms/Blocks/Block.cs
+++ src/Behlog.Cms/Blocks/Block.cs
@@ -1,5 +1,9 @@
+using Behlog.Cms.Commands;
 using Behlog.Core;
+using Behlog.Core.Contracts;
 using Behlog.Core.Domain;
+using Behlog.Extensions;
+using Idyfa.Core.Contracts;
 
 namespace Behlog.Cms.Domain;
 
@@ -14,20 +18,20 @@ public class Block : AggregateRoot<Guid>, IHasMetadata
     public string Title { get; protected set; }
     public string BlockType { get; protected set; }
     public string Category { get; protected set; }
-    public string Author { get; protected set; }
-    public string AuthorEmail { get; protected set; }
-    public string Description { get; protected set; }
-    public string IconName { get; protected set; }
-    public string CoverPhoto { get; protected set; }
+    public string? Author { get; protected set; }
+    public string? AuthorEmail { get; protected set; }
+    public string? Description { get; protected set; }
+    public string? IconName { get; protected set; }
+    public string? CoverPhoto { get; protected set; }
     public string Template { get; protected set; }
-    public string Attributes { get; protected set; }
-    public string Example { get; protected set; }
+    public string? Attributes { get; protected set; }
+    public string? Example { get; protected set; }
     public bool IsRtl { get; protected set; }
     public Guid LangId { get; protected set; }
-    public string Keywords { get; protected set; }
+    public string? Keywords { get; protected set; }
     public Guid? ParentId { get; protected set; }
     public BlockStatus Status { get; protected set; }
-    public string ViewPath { get; protected set; }
+    public string? ViewPath { get; protected set; }
     
     public DateTime CreatedDate { get; protected set; }
     public DateTime? LastUpdated { get; protected set; }
@@ -43,6 +47,50 @@ public class Block : AggregateRoot<Guid>, IHasMetadata
 
     public ICollection<BlockMeta> Meta { get; protected set; }
         = new HashSet<BlockMeta>();
+
+    #endregion
+
+
+    #region Builders
+
+
+    public static Block Create(
+        CreateBlockCommand command, IIdyfaUserContext userContext,
+        IBehlogApplicationContext appContext, ISystemDateTime dateTime)
+    {
+        command.ThrowExceptionIfArgumentIsNull(nameof(command));
+        userContext.ThrowExceptionIfArgumentIsNull(nameof(userContext));
+        appContext.ThrowExceptionIfArgumentIsNull(nameof(appContext));
+        dateTime.ThrowExceptionIfArgumentIsNull(nameof(dateTime));
+
+        var block = new Block
+        {
+            Id = Guid.NewGuid(),
+            Attributes = command.Attributes,
+            Author = command.Author?.CorrectYeKe().Trim()!,
+            Category = command.Category?.Trim()!,
+            Description = command.Description?.CorrectYeKe().Trim()!,
+            Example = command.Example,
+            Keywords = command.Keywords?.CorrectYeKe()!,
+            Name = command.Name?.Trim()!,
+            Status = BlockStatus.Enabled,
+            Template = command.Template,
+            Title = command.Title?.CorrectYeKe().Trim()!,
+            BlockType = command.BlockType?.Trim()!,
+            AuthorEmail = command.AuthorEmail?.Trim()!,
+            CreatedDate = dateTime.UtcNow,
+            CoverPhoto = command.CoverPhoto,
+            IconName = command.IconName,
+            IsRtl = command.IsRtl,
+            LangId = command.LangId,
+            ParentId = command.ParentId,
+            ViewPath = command.ViewPath,
+            CreatedByIp = appContext.IpAddress!
+        };
+
+
+        return block;
+    }
 
     #endregion
 }
