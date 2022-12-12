2d2d28ea67bf7e7ee3004ea3e3749eb56d07554e
imun <imun22@gmail.com>
2022-12-12 16:56:09 +0330

    Add FileUploadQueryHandlers

---
+++ src/Behlog.Cms/FileUploads/Handlers/FileUploadQueryHandlers.cs
@@ -0,0 +1,35 @@
+using Behlog.Cms.Models;
+using Behlog.Cms.Query;
+using Behlog.Cms.Store;
+using Behlog.Core;
+using Behlog.Core.Models;
+using Behlog.Extensions;
+
+namespace Behlog.Cms.Handlers;
+
+
+public class FileUploadQueryHandlers :
+    IBehlogQueryHandler<QueryFileUploadsByWebsiteId, QueryResult<FileUploadResult>>
+{
+    private readonly IFileUploadReadStore _readStore;
+
+
+    public FileUploadQueryHandlers(IFileUploadReadStore readStore)
+    {
+        _readStore = readStore ?? throw new ArgumentNullException(nameof(readStore));
+    }
+    
+    public async Task<QueryResult<FileUploadResult>> HandleAsync(
+        QueryFileUploadsByWebsiteId query, CancellationToken cancellationToken = default)
+    {
+        query.ThrowExceptionIfArgumentIsNull(nameof(query));
+
+        var files = await _readStore.GetFilesAsync(query, cancellationToken).ConfigureAwait(false);
+        
+        return QueryResult<FileUploadResult>.Create()
+            .WithPageNumber(query.Filter.PageNumber)
+            .WithPageSize(query.Filter.PageSize)
+            .WithTotalCount(files.TotalCount)
+            .WithResults(files.Results.Select(_=> _.ToResult()).ToList());
+    }
+}
