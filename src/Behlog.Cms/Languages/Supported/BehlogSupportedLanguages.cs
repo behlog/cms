687f2e504604be947418655991b24a9cb853d296
imun <imun22@gmail.com>
2023-02-01 16:45:45 +0330

    Add GetTitle for SupportedLanguages

--- src/Behlog.Cms/Languages/Supported/BehlogSupportedLanguages.cs
+++ src/Behlog.Cms/Languages/Supported/BehlogSupportedLanguages.cs
@@ -6,37 +6,48 @@ namespace Behlog.Cms;
 public static class BehlogSupportedLanguages
 {
     
-    public static Dictionary<string, Guid> _codeToId = new()
+    private static Dictionary<string, Guid> _codeToId = new()
     {
         { PersianLanguage.Code, PersianLanguage.Id },
         { EnglishLanguage.Code, EnglishLanguage.Id }
     };
 
-    public static Dictionary<string, Guid> _nameToId = new()
+    private static Dictionary<string, Guid> _nameToId = new()
     {
         { PersianLanguage.Name, PersianLanguage.Id },
         { EnglishLanguage.Name, EnglishLanguage.Id }
     };
 
-    public static Dictionary<string, Guid> _isoCodeToId = new()
+    private static Dictionary<string, Guid> _isoCodeToId = new()
     {
         { PersianLanguage.IsoCode, PersianLanguage.Id },
         { EnglishLanguage.IsoCode, EnglishLanguage.Id }
     };
 
-    public static Dictionary<Guid, string> _idToCode = new()
+    private static Dictionary<Guid, string> _idToCode = new()
     {
         { PersianLanguage.Id, PersianLanguage.Code },
         { EnglishLanguage.Id, EnglishLanguage.Code }
     };
 
-    public static Dictionary<Guid, string> _idToName = new()
+    private static Dictionary<Guid, string> _idToName = new()
     {
         { PersianLanguage.Id, PersianLanguage.Name },
         { EnglishLanguage.Id, EnglishLanguage.Name }
     };
 
+    private static Dictionary<string, string> _codeToTitle = new()
+    {
+        { PersianLanguage.Code, PersianLanguage.Title },
+        { EnglishLanguage.Code, EnglishLanguage.Title }
+    };
 
+    private static Dictionary<Guid, string> _idToTitle = new()
+    {
+        { PersianLanguage.Id, PersianLanguage.Title },
+        { EnglishLanguage.Id, EnglishLanguage.Title }
+    };
+    
     public static Guid GetIdByCode(string code) => _codeToId[code.ToLower()];
 
     public static Guid GetIdByName(string name) => _nameToId[name.ToLower()];
@@ -46,4 +57,9 @@ public static class BehlogSupportedLanguages
     public static string GetCodeById(Guid id) => _idToCode[id];
 
     public static string GetNameById(Guid id) => _idToName[id];
+
+    public static string GetTitleByCode(string code) => _codeToTitle[code];
+
+    public static string GetTitleById(Guid id) => _idToTitle[id];
+
 }
