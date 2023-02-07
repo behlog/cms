9b91acfbe7abd2dee552697c1b83f7001ee983e2
imun <imun22@gmail.com>
2023-02-07 11:53:42 +0330

    remove redundant FileType enumeration class

--- src/Behlog.Cms/FileUploads/FileType.cs
+++ src/Behlog.Cms/FileUploads/FileType.cs
@@ -1,5 +1,3 @@
-using Behlog.Core;
-
 namespace Behlog.Cms.Domain;
 
 public enum FileTypeEnum
@@ -11,35 +9,4 @@ public enum FileTypeEnum
     Document = 4,
     Downloads = 5,
     CompressedArchive = 6
-}
-
-public class FileType : Enumeration
-{
-    public FileType(int id, string name, string title = "") 
-        : base(id, name, title)
-    {
-    }
-
-    public static FileType Find(int id) => FromValue<FileType>(id);
-    
-    public static FileType Common
-        => new FileType(0, nameof(Common));
-
-    public static FileType Image
-        => new FileType(1, nameof(Image));
-
-    public static FileType Video
-        => new FileType(2, nameof(Video));
-
-    public static FileType Audio
-        => new FileType(3, nameof(Audio));
-
-    public static FileType Document
-        => new FileType(4, nameof(Document));
-
-    public static FileType Downloads
-        => new FileType(5, nameof(Downloads));
-
-    public static FileType CompressedArchive
-        => new FileType(6, nameof(CompressedArchive));
 }
