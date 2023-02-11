bfb875650c4161b282522e751da78b69ca9dacc0
imaun <imun22@gmail.com>
2023-02-12 00:09:50 +0330

    Add Builders to WebsiteTag

--- src/Behlog.Cms/Website/WebsiteTag.cs
+++ src/Behlog.Cms/Website/WebsiteTag.cs
@@ -19,6 +19,26 @@
 		public Tag Tag { get; protected set; }
 		#endregion
 
+		public static WebsiteTag New(Guid websiteId, Guid tagId) {
+			var websiteTag = new WebsiteTag();
+			websiteTag.WebsiteId = websiteId;
+			websiteTag.TagId = tagId;
+			return websiteTag;
+		}
 
+		public WebsiteTag WithLangId(Guid langId) {
+			LangId = langId;
+			return this;
+		}
+
+		public WebsiteTag WithTagTitle(string tagTitle) {
+			TagTitle = tagTitle;
+			return this;
+		}
+
+		public WebsiteTag WithTagSlug(string tagSlug) {
+			TagSlug = tagSlug;
+			return this;
+		}
 	}
 }
