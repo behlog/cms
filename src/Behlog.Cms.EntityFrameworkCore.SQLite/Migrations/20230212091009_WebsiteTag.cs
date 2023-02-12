502580c89eafa0213cf6f8b017c0c20294fb031e
imun <imun22@gmail.com>
2023-02-12 12:48:45 +0330

    Add-Migration WebsiteTag

---
+++ src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230212091009_WebsiteTag.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Behlog.Cms.EntityFrameworkCore.SQLite.Migrations
+{
+    public partial class WebsiteTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebsiteTag",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    WebsiteId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TagId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    LangId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TagTitle = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    TagSlug = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebsiteTag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebsiteTag_Language_LangId",
+                        column: x => x.LangId,
+                        principalTable: "Language",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WebsiteTag_Tag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WebsiteTag_Website_WebsiteId",
+                        column: x => x.WebsiteId,
+                        principalTable: "Website",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteTag_LangId",
+                table: "WebsiteTag",
+                column: "LangId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteTag_TagId",
+                table: "WebsiteTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteTag_WebsiteId",
+                table: "WebsiteTag",
+                column: "WebsiteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebsiteTag");
+        }
+    }
+}
