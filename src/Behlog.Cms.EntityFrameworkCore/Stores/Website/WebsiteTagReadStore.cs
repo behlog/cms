c08dc81c831ec81fb6e2b3b74ce68ff27bd20334
imun <imun22@gmail.com>
2023-02-14 18:04:53 +0330

    Implement WebsiteTagReadStore

--- src/Behlog.Cms.EntityFrameworkCore/Stores/Website/WebsiteTagReadStore.cs
+++ src/Behlog.Cms.EntityFrameworkCore/Stores/Website/WebsiteTagReadStore.cs
@@ -13,30 +13,52 @@ public class WebsiteTagReadStore : IWebsiteTagReadStore
     public WebsiteTagReadStore(IBehlogEntityFrameworkDbContext db)
     {
         _db = db ?? throw new ArgumentNullException(nameof(db));
+        _set = _db.Set<WebsiteTag>();
     }
 
-    public Task<bool> AnyAsync(Guid websiteId, Guid tagId, Guid contentId, CancellationToken cancellationToken = default)
+    public async Task<bool> AnyAsync(
+        Guid websiteId, Guid tagId, Guid contentId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return await _set.AnyAsync(_ => _.WebsiteId == websiteId &&
+                                        _.TagId == tagId && 
+                                        _.ContentId == contentId, 
+            cancellationToken).ConfigureAwait(false);
     }
 
-    public Task<IReadOnlyCollection<WebsiteTag>> FindAllAsync(Guid websiteId, CancellationToken cancellationToken = default)
+    public async Task<IReadOnlyCollection<WebsiteTag>> FindAllAsync(
+        Guid websiteId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return (await _set
+            .Where(_ => _.WebsiteId == websiteId)
+            .GroupBy(_ => _.TagId)
+            .Select(_ => _.FirstOrDefault())
+            .ToListAsync(cancellationToken).ConfigureAwait(false))!;
     }
 
-    public Task<WebsiteTag> FindAsync(long id)
+    public async Task<WebsiteTag> FindAsync(long id)
     {
-        throw new NotImplementedException();
+        return (await _set.FindAsync(id).ConfigureAwait(false))!;
     }
 
-    public Task<IReadOnlyCollection<WebsiteTag>> FindAllAsync(Guid websiteId, Guid langId, CancellationToken cancellationToken = default)
+    public async Task<IReadOnlyCollection<WebsiteTag>> FindAllAsync(
+        Guid websiteId, Guid langId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return (await _set
+            .Where(_ => _.WebsiteId == websiteId)
+            .Where(_ => _.LangId == langId)
+            .GroupBy(_ => _.TagId)
+            .Select(_ => _.FirstOrDefault())
+            .ToListAsync(cancellationToken).ConfigureAwait(false))!;
     }
 
-    public Task<IReadOnlyCollection<WebsiteTag>> FindByContentTypeAsync(Guid websiteId, Guid contentTypeId, CancellationToken cancellationToken = default)
+    public async Task<IReadOnlyCollection<WebsiteTag>> FindByContentTypeAsync(
+        Guid websiteId, Guid contentTypeId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return (await _set
+            .Where(_ => _.WebsiteId == websiteId)
+            .Where(_ => _.ContentTypeId == contentTypeId)
+            .GroupBy(_ => _.TagId)
+            .Select(_ => _.FirstOrDefault())
+            .ToListAsync(cancellationToken).ConfigureAwait(false))!;
     }
 }
