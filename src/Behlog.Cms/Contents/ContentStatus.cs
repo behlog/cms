512a21130c5546d977313d3d3983a5232559ae3c
imuan <imun22@gmail.com>
2023-05-23 17:50:47 +0330

    add ToDisplay ext for ContentStatus

--- src/Behlog.Cms/Contents/ContentStatus.cs
+++ src/Behlog.Cms/Contents/ContentStatus.cs
@@ -1,4 +1,5 @@
 using Behlog.Core;
+using Behlog.Cms.Resources;
 
 namespace Behlog.Cms.Domain;
 
@@ -8,4 +9,18 @@ public enum ContentStatusEnum
     Draft = 0,
     Published = 1,
     Planned = 2
+}
+
+public static class ContentStatusExtensions
+{
+
+    public static string ToDisplay(this ContentStatusEnum status) 
+        => status switch
+        {
+            ContentStatusEnum.Deleted => "Deleted", //TODO : Read from resource
+            ContentStatusEnum.Draft => "Draft",
+            ContentStatusEnum.Planned => "Planned",
+            ContentStatusEnum.Published => "Published",
+            _ => "Unknown"
+        };
 }
