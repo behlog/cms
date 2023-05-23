63f853efd203156ce0271c6c952096797676dfaf
imuan <imun22@gmail.com>
2023-05-23 17:15:04 +0330

    add-migration ContentHistory

---
+++ src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230523133948_ContentHistory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Behlog.Cms.EntityFrameworkCore.SQLite.Migrations
+{
+    public partial class ContentHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContentHistory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContentId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    AltTitle = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    Slug = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    BodyType = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 1),
+                    Body = table.Column<string>(type: "nTEXT", nullable: true),
+                    Summary = table.Column<string>(type: "TEXT", maxLength: 2000, nullable: true),
+                    UserId = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    UserDisplayName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
+                    UserName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
+                    IpAddress = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContentHistory_Content_ContentId",
+                        column: x => x.ContentId,
+                        principalTable: "Content",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentHistory_ContentId",
+                table: "ContentHistory",
+                column: "ContentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContentHistory");
+        }
+    }
+}
