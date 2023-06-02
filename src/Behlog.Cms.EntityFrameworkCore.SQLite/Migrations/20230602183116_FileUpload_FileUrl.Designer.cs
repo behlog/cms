674d5868a58f58ec0bd3aae92a78a24def755a78
imaun <imun22@gmail.com>
2023-06-02 22:02:15 +0330

    add-migration FileUpload_FileUrl for SQLite EFCore

--- src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230602183116_FileUpload_FileUrl.Designer.cs
+++ src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230602183116_FileUpload_FileUrl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Behlog.Cms.EntityFrameworkCore.SQLite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Behlog.Cms.EntityFrameworkCore.SQLite.Migrations
 {
     [DbContext(typeof(BehlogSQLiteDbContext))]
-    partial class BehlogSQLiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230602183116_FileUpload_FileUrl")]
+    partial class FileUpload_FileUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
---
+++ src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230602183116_FileUpload_FileUrl.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Behlog.Cms.EntityFrameworkCore.SQLite.Migrations
+{
+    public partial class FileUpload_FileUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileUrl",
+                table: "FileUpload",
+                type: "TEXT",
+                maxLength: 4000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileUrl",
+                table: "FileUpload");
+        }
+    }
+}
