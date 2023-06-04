c4f162ed7f54bb2e583832382b40dc8cc6b9c401
imaun <imun22@gmail.com>
2023-06-04 22:28:28 +0330

    add-migration File_AltFileUrl

---
+++ src/Behlog.Cms.EntityFrameworkCore.SQLite/Migrations/20230604185745_File_AltFileUrl.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Behlog.Cms.EntityFrameworkCore.SQLite.Migrations
+{
+    public partial class File_AltFileUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AltFileSize",
+                table: "FileUpload",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AltFileUrl",
+                table: "FileUpload",
+                type: "TEXT",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AltFileSize",
+                table: "FileUpload");
+
+            migrationBuilder.DropColumn(
+                name: "AltFileUrl",
+                table: "FileUpload");
+        }
+    }
+}
