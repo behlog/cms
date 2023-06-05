42520a3cc55d2410497cf30a6d7383af1ce68c0c
imaun <imun22@gmail.com>
2023-06-05 19:52:09 +0330

    add-migration FileUpload_ContentType SQL Server

---
+++ src/Behlog.Cms.EntityFrameworkCore.SqlServer/Migrations/20230605162129_FileUpload_ContentType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Behlog.Cms.EntityFrameworkCore.SqlServer.Migrations
+{
+    public partial class FileUpload_ContentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "FileUpload",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "FileUpload");
+        }
+    }
+}
