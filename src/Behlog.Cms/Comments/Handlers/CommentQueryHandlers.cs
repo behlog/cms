f9faca7c11675c1594c0e9ac4ca77fa72d0637be
imaun <imun22@gmail.com>
2023-06-16 23:55:54 +0330

    add Handler for QueryWebsiteComments

---
+++ src/Behlog.Cms/Comments/Handlers/CommentQueryHandlers.cs
@@ -0,0 +1,26 @@
+﻿namespace Behlog.Cms.Handlers;
+
+public class CommentQueryHandlers :
+    IBehlogQueryHandler<QueryWebsiteComments, QueryResult<CommentResult>>
+{
+    private readonly ICommentReadStore _readStore;
+
+    public CommentQueryHandlers(ICommentReadStore readStore)
+    {
+        _readStore = readStore ?? throw new ArgumentNullException(nameof(readStore));
+    }
+    
+    public async Task<QueryResult<CommentResult>> HandleAsync(
+        QueryWebsiteComments query, CancellationToken cancellationToken = default)
+    {
+        query.ThrowExceptionIfArgumentIsNull(nameof(query));
+
+        var result = await _readStore.QueryAsync(query, cancellationToken).ConfigureAwait(false);
+        
+        return QueryResult<CommentResult>
+            .Create(result.Results.Select(_=> _.ToResult()))
+            .WithPageNumber(result.PageNumber)
+            .WithPageSize(result.PageSize)
+            .WithTotalCount(result.TotalCount);
+    }
+}
