86fbe21d5029509ef9804fe749ede1f2b154d07f
imaun <imun22@gmail.com>
2023-06-16 22:49:34 +0330

    Add QueryWebsiteComments model

--- src/Behlog.Cms/Comments/ICommentReadStore.cs
+++ src/Behlog.Cms/Comments/ICommentReadStore.cs
@@ -1,8 +1,15 @@
-using Behlog.Core;
-
 namespace Behlog.Cms.Domain;
 
 public interface ICommentReadStore : IBehlogReadStore<Comment, Guid>
 {
     
+    /// <summary>
+    /// Query for <see cref="Comment"/> in a specified <see cref="Website"/>
+    /// with filter and pagination support.
+    /// </summary>
+    /// <param name="query"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<QueryResult<Comment>> QueryAsync(
+        QueryWebsiteComments query, CancellationToken cancellationToken = default);
 }
---
+++ src/Behlog.Cms/Comments/Query/QueryWebsiteComments.cs
@@ -0,0 +1,21 @@
+﻿namespace Behlog.Cms.Query;
+
+
+public class QueryWebsiteComments : IBehlogQuery<QueryResult<CommentResult>>
+{
+    public QueryWebsiteComments(
+        Guid websiteId, CommentStatusEnum? status, QueryOptions? options = null)
+    {
+        WebsiteId = websiteId;
+        Status = status;
+        Options = options ?? QueryOptions.New()
+            .WillOrderBy("id")
+            .WithPageSize(1)
+            .WithPageSize(10)
+            .WillOrderDesc();
+    }
+    
+    public Guid WebsiteId { get; }
+    public CommentStatusEnum? Status { get; }
+    public QueryOptions Options { get; }
+}
